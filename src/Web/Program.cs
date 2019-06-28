9c707de20bfcc27f65ffe6ed6ff2e0206850f65c
Tsuyoshi Ushio <tsushi@microsoft.com>
2019-06-28 19:50:30 +0000

    Add new lines of code

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -54,6 +54,29 @@ namespace Microsoft.eShopWeb.Web
             }
         }
 
+        public void NewMethod() {
+                        using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var catalogContext = services.GetRequiredService<CatalogContext>();
+                    CatalogContextSeed.SeedAsync(catalogContext, loggerFactory)
+            .Wait();
+
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    AppIdentityDbContextSeed.SeedAsync(userManager).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+        }
+
         public static string ChangePassword = "Password@1";
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
