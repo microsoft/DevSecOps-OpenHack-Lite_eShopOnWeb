05885bd3b62d41d92d3b142eab8711a19844adb0
Tsuyoshi Ushio <tsushi@microsoft.com>
2019-07-01 23:13:13 +0000

    remove fail to build

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -39,8 +39,34 @@ namespace Microsoft.eShopWeb.Web
             host.Run();
         }
 
-        public void Unused() {
-            
+        public void Unused(int a) {
+            if (a == 1) {
+                 Console.WriteLine("one");
+                 
+                 Console.WriteLine("one");
+                 
+                 Console.WriteLine("one");
+                 
+                 Console.WriteLine("one");
+                 
+                 Console.WriteLine("one");
+                 
+                 Console.WriteLine("one");
+            } else {
+                 Console.WriteLine("not one");
+
+                 Console.WriteLine("not one");
+
+                 Console.WriteLine("not one");
+
+                 Console.WriteLine("not one");
+
+                 Console.WriteLine("not one");
+
+                 Console.WriteLine("not one");
+
+                 Console.WriteLine("not one");
+            }
         }
 
         public void Execute()
@@ -55,25 +81,7 @@ namespace Microsoft.eShopWeb.Web
         }
 
         public void NewMethod() {
-                        using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-                try
-                {
-                    var catalogContext = services.GetRequiredService<CatalogContext>();
-                    CatalogContextSeed.SeedAsync(catalogContext, loggerFactory)
-            .Wait();
 
-                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                    AppIdentityDbContextSeed.SeedAsync(userManager).Wait();
-                }
-                catch (Exception ex)
-                {
-                    var logger = loggerFactory.CreateLogger<Program>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
 
         }
 
